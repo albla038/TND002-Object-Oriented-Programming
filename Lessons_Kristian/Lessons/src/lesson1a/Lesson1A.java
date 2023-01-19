9b98e5904d09c45f71a62e991d26482dff08baa1
QueueSecret <kriho388@student.liu.se>
2023-01-19 18:02:45 +0100

    lesson1a with notes

--- Lessons_Kristian/Lessons/src/lesson1a/Lesson1A.java
+++ Lessons_Kristian/Lessons/src/lesson1a/Lesson1A.java
@@ -4,29 +4,43 @@ public class Lesson1A {
 	
 	public static void main(String[] args) {
 		System.out.println("Lesson 1A\n—————————"); 
-		double d1 = 4.0;
+		double d1 = 4.0; //double precision
 		System.out.println("The double value is " + d1);
 		int i1 = 10;
 		System.out.println("The sum of the double value and the integer value is " + (d1 + i1));
 		System.out.println(d1 + i1 + " is the sum of the double value and the integer value.");
-		int i2 =(int) d1;
-		System.out.println("Result of the cast is: " + i2);
-		boolean b1 = true;
-		System.out.println(b1);
-		final double d2 = 2.0;
-		final double d3;
-		d3 = 5.0;
-		System.out.println(d3);
+
+		String s2 = "4.0";
 		
+		double d2 = 3.0;
+		
+		String s3 = String.valueOf(d2);
+		
+		System.out.println(s3);
+		
+//		int i2 =(int) d1;
+//		System.out.println("Result of the cast is: " + i2);
+//		final double d3;
+//		d3 = 5.0;
+//		System.out.println(d3);
+//		
 		System.out.println(Manipulations.convert(1));
 		
 		System.out.println(Manipulations.convert(4.0));
+//		
+//		Manipulations manip = new Manipulations();
+//		
+//		System.out.println(manip);
+//		
+//		Manipulations.convert();
 		
-		Manipulations manip = new Manipulations();
+		//Can make a dummy of a main "Lesson1A dummy = new Lesson1A();
 		
-		System.out.println(manip);
+		//Start debugging at first own implemented class
 		
-		Manipulations.convert();
+		//uninitialized constants can be changed once
+		
+		//Local and global scope, global might have access modifier by default (public)
 	}
 	
 }
