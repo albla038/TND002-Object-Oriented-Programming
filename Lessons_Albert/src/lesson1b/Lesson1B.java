3df565bfbe86c72868ec4216dd2f68aaf1c0a9c1
Albert0002 <albert.lannerstrom@gmail.com>
2023-01-22 15:52:46 +0100

    lesson1b done

--- Lessons_Albert/src/lesson1b/Lesson1B.java
+++ Lessons_Albert/src/lesson1b/Lesson1B.java
@@ -3,8 +3,75 @@ package lesson1b;
 public class Lesson1B {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ManipulationsB.setSpecies("Human");
+		System.out.println(ManipulationsB.getSpecies());
+		
+		ManipulationsB human1 = new ManipulationsB();
+		human1.setName("Paul");
+		
+		ManipulationsB human2 = new ManipulationsB();
+		human2.setName("Astrid");
+		
+		System.out.println(ManipulationsB.getSpecies() + " - " + human1.getName());
+		System.out.println(ManipulationsB.getSpecies() + " - " + human2.getName());
+	
+		/*
+		String temp = human1.getName();
+		human1.setName(human2.getName());
+		human2.setName(temp);
+		*/
+		
+		ManipulationsB.staticSwap(human1, human2);
+		
+		System.out.println("-----");
+		
+		System.out.println(ManipulationsB.getSpecies() + " - " + human1.getName());
+		System.out.println(ManipulationsB.getSpecies() + " - " + human2.getName());
+		
+		human1.instanceSwap(human2);
 
+		System.out.println("-----");
+		
+		System.out.println(ManipulationsB.getSpecies() + " - " + human1.getName());
+		System.out.println(ManipulationsB.getSpecies() + " - " + human2.getName());
+		
+		int days = 365;
+		double number = 654.321;
+		String text = String.format("One year has %1d days", days);
+		System.out.println("\n"+ text);
+		
+		text = String.format("The formatted number %7.3f is %1.1f", number, number);
+		System.out.println("\n"+ text);
+		
+		String myText = "year";
+		text = String.format("One %4s has %1d days", myText, days);
+		System.out.println("\n"+ text);
+		
+		String severalNames = "Paul Astrid,Thomas Sybill";
+		String[] strarray = severalNames.split(",");
+		System.out.println(strarray.length);
+		
+		human1 = new ManipulationsB("Paul");
+		human1 = new ManipulationsB("Astrid");
+		
+		System.out.println(human1);
+		
+		ManipulationsB dog = new ManipulationsB("Barky", "Dog");
+	
+		System.out.println(dog);
+		System.out.println(human1);
+		
+		String input = "Paul Anki Cesar Miranda";
+		String[] nameArray = input.split(" ");
+		ManipulationsB[] people = new ManipulationsB[nameArray.length];
+	
+		System.out.println("\n\n----");
+		
+		for (int i = 0; i < nameArray.length; i++) {
+			people[i] = new ManipulationsB(nameArray[i]);
+			System.out.println(people[i]);
+		}
 	}
 
 }
--- Lessons_Albert/src/lesson1b/ManipulationsB.java
+++ Lessons_Albert/src/lesson1b/ManipulationsB.java
@@ -1,5 +1,60 @@
 package lesson1b;
 
 public class ManipulationsB {
+	
+	private static String species;
+	
+	private String name;
+	
+	public ManipulationsB() {
+		name = "";
+		species = "Human";
+		System.out.print("Default constructor was called " + name + species + "\n");
+	}
+	
+	public ManipulationsB(String arg) {
+		name = arg;
+		species = "Human";
+		System.out.print("Default constructor was called " + name + species + "\n");
+	}
+	
+	public ManipulationsB(String arg1, String arg2) {
+		name = arg1;
+		species = arg2;
+		System.out.print("Default constructor was called " + name + species + "\n");
+	}
+	
+	public static void staticSwap(ManipulationsB arg1, ManipulationsB arg2) {
+		String temp = arg1.getName();
+		arg1.setName(arg2.getName());
+		arg2.setName(temp);
+	}
+	
+	public static void setSpecies(String arg) {
+		species = arg;
+	}
+	
+	public static String getSpecies() {
+		return species;
+	}
+	
+	public void instanceSwap(ManipulationsB arg) {
+		String temp = getName();
+		setName(arg.getName());
+		arg.setName(temp);
+	}
+	
+	public void setName(String arg) {
+		name = arg;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		String result = String.format("Species: %-10s, Name: %10s", species, name);
+		return result;
+	}
 
 }
