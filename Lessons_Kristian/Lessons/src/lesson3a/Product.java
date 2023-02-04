01a0cbfb32c0d1b5d87ea2acda9187d52afb4b94
QueueSecret <kriho388@student.liu.se>
2023-02-04 16:57:55 +0100

    Lesson3a finished

--- Lessons_Kristian/Lessons/src/lesson3a/Product.java
+++ Lessons_Kristian/Lessons/src/lesson3a/Product.java
@@ -1,5 +1,102 @@
 package lesson3a;
 
-public class Product {
+import java.util.ArrayList;
 
+public class Product {
+	public final String NAME;
+	public static final String[] ALLSTORES = {"Norrköping", "Linköping", "Stockholm", "Malmö"};
+	private Double userScore = null;
+	private /**/static ArrayList<String> availableStores;
+	public String description;
+	private /**/static ArrayList<String> stores = new ArrayList<String>(); //must be initialized?
+	
+	public Product(String arg) {
+		NAME = arg;
+		convertArray();
+	}
+	public Product(String arg1, Double arg2) {
+		NAME = arg1; userScore = arg2;
+		convertArray();
+	}
+	
+	private void convertArray() {
+		//availableStores = new ArrayList<String>();
+		//for (int i = 0; i < ALLSTORES.length; i++;){
+		//	availableStores.add(ALLSTORES[i]);
+		//}
+		availableStores = new ArrayList<String>();
+		for(String store : ALLSTORES) {
+		availableStores.add(store);
+		}
+	}
+	
+	public static String listAvailableStores(){
+		String result = "Number, City\n";
+		
+		//for (int i=0; i < availableStores.size(); i++) {
+		//	result = result + i + " " + availableStores.get(i);
+		//}
+		
+		for(String store : availableStores) {
+			result += availableStores.indexOf(store) + " " + store + "\n";
+		}
+		
+		return result;
+	}
+	
+	public static String selectStore(int arg) {
+		
+		//if (arg < availableStores.size()) {
+		//	String temp = availableStores.get(arg);
+		//	stores.add(temp);
+		//	availableStores.remove(arg);
+		//	return temp + "added";
+		//}
+		
+		if(arg < availableStores.size() && arg >= 0) {
+			//stores.add(availableStores.remove(arg)); //Can't display removed store
+			String temp = availableStores.get(arg);
+			stores.add(temp);
+			availableStores.remove(arg);
+			return temp + " added";
+		} else {
+			return "Wrong number";
+		}
+	}
+	
+	public String setDescription(String arg) {
+		description = arg;
+		return "Product description done";
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getRating() {
+		if (userScore == null) {
+			return "Not rated yet";
+		} else {
+			return userScore.toString();
+		}
+	}
+	
+	public String toString() {
+		String result = "This product ";
+		if (userScore != null) {
+			result += "has the user score " + userScore + " and ";
+		}
+		result += "is available in the shops in: ";
+		for(int i = 0; i < stores.size(); i++) {
+			result += stores.get(i);
+			if (i == stores.size() - 2) {
+				result += " and ";
+			} else if (i == stores.size() - 1){
+				result += ".";
+			} else {
+				result += ", ";
+			}
+		}
+		return result;
+	}
 }
--- Lessons_Kristian/Lessons/src/lesson3a/TVSet.java
+++ Lessons_Kristian/Lessons/src/lesson3a/TVSet.java
@@ -1,5 +1,59 @@
 package lesson3a;
 
-public class TVSet extends Product{
+import java.util.ArrayList;
+import java.util.Random;
 
+public class TVSet extends Product{
+	private String brand, description;
+	private int price;
+	public static ArrayList<TVSet> allTVs = new ArrayList<TVSet>();
+	
+	//needs default constructor in superclass with no args
+	//in order to use default constructor in subclass
+	
+	public TVSet(String arg) {
+		super("tv set"); //calls superclass constructor with arg "tv set", must be first line
+		brand = arg;
+		Random rand = new Random();
+		price = 10000 + rand.nextInt(5001);
+		allTVs.add(this); //adds created object to allTVs
+	}
+	
+	public TVSet(String arg1, Double arg2) {
+		super("tv set", arg2);
+		brand = arg1;
+		Random rand = new Random();
+		price = 10001 + rand.nextInt(5000);
+		allTVs.add(this);
+	}
+	
+	public String setDescription(String arg) {
+		String temp[] = arg.split(",");
+		description = temp[1].trim();;
+		
+		return super.setDescription(temp[0].trim());
+	}
+	
+	public String getDescription() {
+		return "TV description: " + description + "\nProduct information: " + super.description;
+	}
+	
+	public static void listTVs() {
+		for (TVSet tvInfo : allTVs) {
+			String result = tvInfo.brand + " " + tvInfo.price + " " + tvInfo.getRating();
+			System.out.println(result);
+		}
+	}
+	
+	public String toString() {
+		String result = "The " + NAME + " is manuafactured by " + brand + " and costs " + price + "SEK\n";
+		
+		return result + super.toString();
+	}
+	
+	//public String toString(){
+	//	return super.toString() + "TVSet";
+	//}
+	
+	//super taking the place of super, used to reference superclass
 }
