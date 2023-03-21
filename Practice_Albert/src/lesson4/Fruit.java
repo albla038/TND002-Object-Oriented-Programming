e245db7c0cc784e9eb3f7dc20707fbe343392443
Albert0002 <albert.lannerstrom@gmail.com>
2023-03-21 13:33:44 +0100

    Added inner static class 'Strawberry'

--- Practice_Albert/src/lesson4/Fruit.java
+++ Practice_Albert/src/lesson4/Fruit.java
@@ -17,4 +17,23 @@ public class Fruit {
 	public String toString() {
 		return String.format("Specifics: %7s, Price: %8.2", specifics, price);
 	}
+	
+	public static class Strawberry {
+		private String name, specifics;
+		private double price;
+		
+		public Strawberry(String arg1, String arg2, double arg3) {
+			name = arg1;
+			specifics = arg2;
+			price = arg3;
+		}
+		
+		public String computeCost(int arg) {
+			return "Strawberry" + arg * price;
+		}
+		
+		public String toString() {
+			return new Fruit(specifics, price).toString();
+		}
+	}
 }
