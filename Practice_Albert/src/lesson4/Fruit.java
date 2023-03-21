3a9850ccf3c4343661740defe0ba8d2b873599bf
Albert0002 <albert.lannerstrom@gmail.com>
2023-03-21 10:31:23 +0100

    Test

--- Practice_Albert/src/lesson4/Fruit.java
+++ Practice_Albert/src/lesson4/Fruit.java
@@ -2,4 +2,19 @@ package lesson4;
 
 public class Fruit {
 
+	private String specifics;
+	private double price;
+	
+	public Fruit(String arg1, double arg2) {
+		specifics = arg1;
+		price = arg2;
+	}
+	
+	public String computeCost(int arg) {
+		return "Fruit" + arg * price;
+	}
+	
+	public String toString() {
+		return String.format("Specifics: %7s, Price: %8.2", specifics, price);
+	}
 }
